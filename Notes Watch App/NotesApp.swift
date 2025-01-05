c9cea18354fb285681f675ac0922f4941843284f
Shubham Kumar <shubhamkumar01@live.com>
2025-01-05 14:45:09 +0530

    Initial Commit

---
+++ Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Shubham Kumar on 05/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Notes Watch AppTests/Notes_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Notes_Watch_AppTests.swift
+//  Notes Watch AppTests
+//
+//  Created by Shubham Kumar on 05/01/25.
+//
+
+import Testing
+@testable import Notes_Watch_App
+
+struct Notes_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
